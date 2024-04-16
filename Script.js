f00398a0f61b095c9bf9e746a33cac479d5c3521
Aman Prakash <amanprakash1324@gmail.com>
2024-04-16 13:04:08 +0530

    Added Spotify

--- Script.js
+++ Script.js
@@ -1,4 +1,19 @@
 console.log('Javascript')
+let currentSong = new Audio();
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "Invalid input";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 async function getSongs(){
 
     let a = await fetch("http://127.0.0.1:5500/Songs/")
@@ -16,8 +31,20 @@ async function getSongs(){
     return songs   
 }
 
+const playMusic = (track, pause=false)=>{
+    //let audio = new Audio("/Songs/" + track)
+   currentSong.src = "/Songs/" + track
+   if(!pause){
+    currentSong.play()
+    play.src = "SVG/pause.svg"
+   }
+   document.querySelector(".songinfo").innerHTML = decodeURI(track)
+   document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
 async function main() {
     let songs = await getSongs()
+    playMusic(songs[0], true)
 
     //Showing song list
 
@@ -36,7 +63,44 @@ async function main() {
     </li>`;
     }
 
-    
+    //Attach song file
+
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click", element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+    })
+
+    // Attach song button to play, pause and next
+
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "SVG/pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "SVG/play.svg"
+        }
+    })
+
+    // Time update function
+    currentSong.addEventListener("timeupdate", ()=>{
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+    })
+
+    // Add an event listener to seekbar
+
+    document.querySelector(".seekbar").addEventListener("click", e => {
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((currentSong.duration) * percent) / 100
+    })
+
+
 }
 
 main()
